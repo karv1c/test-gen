00e6cbb1b2394b8cd781c7ad24697de9578facc1
Виктор Караулов <karv1c@MacBooch.local>
2024-11-01 16:42:11 +0300

    2

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut x = std::fs::OpenOptions::new().create(true).write(true).open("src/out.rs").unwrap();
     x.write_all(string.as_bytes()).unwrap();
 
-    let string = String::from("pub mod out;");
+    let string = String::from("pub mod out;\npub use out::*;");
     let mut x = std::fs::OpenOptions::new().append(true).open("src/lib.rs").unwrap();
     x.write_all(string.as_bytes()).unwrap();
 }
