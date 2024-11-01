e3717013bb29bed90bcf79990b16312f08c137c6
Виктор Караулов <karv1c@MacBooch.local>
2024-11-01 16:35:40 +0300

    init

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::io::Write;
+
+fn main() {
+    let string = String::from("pub struct Test;");
+    let mut x = std::fs::OpenOptions::new().create(true).write(true).open("src/out.rs").unwrap();
+    x.write_all(string.as_bytes()).unwrap();
+
+    let string = String::from("pub mod out;");
+    let mut x = std::fs::OpenOptions::new().append(true).open("src/lib.rs").unwrap();
+    x.write_all(string.as_bytes()).unwrap();
+}
